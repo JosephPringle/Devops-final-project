b752281665eab20af4f72a23e9054e409fae8308
Joseph Pringle <131486669+JosephPringle@users.noreply.github.com>
2026-04-28 19:44:54 -0500

    Configure ARM variables and update container settings
    Added variables for ARM client and Django secret key. Updated container image and port configuration.

--- .github/stage 2/main.tf
+++ .github/stage 2/main.tf
@@ -1,3 +1,7 @@
+variable ARM_CLIENT_IS {}
+variable ARM_CLIENT_SECRET {}
+variable DJANGO_SECRET_KEY_PROD {}
+
 terraform {
   required_providers {
     azurerm = {
@@ -35,12 +39,21 @@ resource "azurerm_container_group" "aci-JosephPringle-acmp" {
 
   container {
     name   = "final-app"
-    image  = "mcr.microsoft.com/azuredocs/final:latest"
+    image  = "acrjosephpringleacmp2400.azurecr.io/final:latest"
     cpu    = "0.5"
     memory = "1.5"
 
     ports {
-      port     = 443
+      port     = 8000
       protocol = "TCP"
     }
+    secure_enviornment_variables {
+        DJANGO_SECRET_KEY = var.DJANGO_SECRET_KEY_PROD
+    }
   }
+  image_registry_credential {
+    server = "acrjosephpringleacmp2400.azurecr.io"
+    username = var.ARM_CLIENT_ID
+    password = var.ARM_CLIENT_SECRET
+  }
+}
